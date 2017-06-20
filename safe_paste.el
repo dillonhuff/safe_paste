83d49ccc9645fe94968c613d516ced2494c95a49
Dillon Huff <dillonhuff@gmail.com>
2017-06-20 13:22:44 -0700

    I now understand how to add highlighting to a mode

--- safe_paste.el
+++ safe_paste.el
@@ -11,6 +11,35 @@
   (interactive)
   (insert (car kill-ring-yank-pointer)))
 
+(defface my-highlight
+       '((((class color) (min-colors 88) (background light))
+          :background "darkseagreen2")
+         (((class color) (min-colors 88) (background dark))
+          :background "darkolivegreen")
+         (((class color) (min-colors 16) (background light))
+          :background "darkseagreen2")
+         (((class color) (min-colors 16) (background dark))
+          :background "darkolivegreen")
+         (((class color) (min-colors 8))
+          :background "green" :foreground "black")
+         (t :inverse-video t))
+       "Basic face for highlighting."
+       :group 'basic-faces)
 
+(font-lock-add-keywords 'emacs-lisp-mode
+			'(("foo" . 'my-highlight)))
 
+;; agressive whitespace marking.
+(defface extra-whitespace-face
+  '((t (:background "pale green")))
+  "Used in text-mode and friends for exactly one space after a period.")
 
+(mapc (lambda (mode)
+        (font-lock-add-keywords
+         mode
+         '(("hello" 0 'show-paren-mismatch-face)
+           ("\\.\\( \\)\\b" 1 'my-highlight))))
+      '(text-mode latex-mode html-mode emacs-lisp-mode
+        texinfo-mode))
+
+(defun dummy () 'foo)
