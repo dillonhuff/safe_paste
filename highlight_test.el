8ca17afaf254783eeb91e0008f9ca12ba112bef1
Dillon Huff <dillonhuff@gmail.com>
2017-06-20 21:18:31 -0700

    Have now copied over essential elements of the highlight test. Need to rewrite so that I can actually develop selection code that relies on marks and copies

--- highlight_test.el
+++ highlight_test.el
@@ -13,6 +13,9 @@
        "Basic face for highlighting."
        :group 'basic-faces)
 
+(defvar highlight-symbol-keyword-alist nil)
+(make-variable-buffer-local 'highlight-symbol-keyword-alist)
+
 (defun highlight-symbol-flush ()
   (if (fboundp 'font-lock-flush)
       (font-lock-flush)
@@ -22,8 +25,20 @@
 
 (defun highlight-symbol-add-symbol-with-face (symbol face)
   (let ((keywords `(,symbol 0 ',face prepend)))
+    (push keywords highlight-symbol-keyword-alist)
     (font-lock-add-keywords nil (list keywords) 'append)
+    (highlight-symbol-flush)))
+
+(defun highlight-symbol-remove-symbol (symbol)
+  (let ((keywords (assoc symbol highlight-symbol-keyword-alist)))
+    (setq highlight-symbol-keyword-alist
+          (delq keywords highlight-symbol-keyword-alist))
+    (font-lock-remove-keywords nil (list keywords))
     (highlight-symbol-flush)))
 
 (highlight-symbol-add-symbol-with-face "font" 'my-highlight)
 (highlight-symbol-add-symbol-with-face "nil" 'my-highlight)
+
+
+(highlight-symbol-remove-symbol "nil")
+(highlight-symbol-remove-symbol "font")
